35f2ee419ec28011ec1e6997a354d1438074736a
Rodrigo <rodrigo.santos@grupoicts.com.br>
2025-02-22 10:28:04 -0400

    feat: swagger initial config

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
